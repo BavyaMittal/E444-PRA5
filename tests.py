39615c0412193563b33cf579bcff6864ab185a8e
BavyaMittal <bavyamittal@gmail.com>
2024-10-25 01:22:19 -0400

    Added tests and testresults

--- tests.py
+++ tests.py
@@ -8,52 +8,50 @@ import matplotlib.pyplot as plt
 # http://ece444-pra5-try2.ca-central-1.elasticbeanstalk.com/ 
 
 # sends the test cases to the api, and records the result & the latency. 
+
 def api_call(url, test):
+
     start = time.time()
+
     result = requests.post(url, test)
+
     end = time.time()
 
     latency = end - start 
-    return result.status_code, latency, result
+
+    return result.status_code, latency 
 
 def boxplot():
+
     df = pd.read_csv("test_results.csv")
 
     plt.figure(figsize=(10, 6))
     df.boxplot(column="Latency (s)", by="Test Nr", grid=False)
-
     plt.title("Latency by Test Nr")
     plt.suptitle("")
     plt.xlabel("Test Nr")
     plt.ylabel("Latency (s)")
-
     plt.savefig("latency_boxplot.png")
     print("Boxplot saved to 'latency_boxplot.png'.")
-
     # Show the plot
     plt.show()
     return
-
 def test():
-    url = "http://ece444-pra5-ml-env.eba-xqkmjmmw.us-east-1.elasticbeanstalk.com/predict"
-    url2 = "http://ece444-pra5-ml-env-2.eba-xqkmjmmw.us-east-1.elasticbeanstalk.com/predict"
-   
-
-    test_text = ["Donald Trump won the 2024 Election", 
-                 "The earth is flat"]
+    url = "http://e444pra-5-env.eba-pkb2wvr5.us-east-2.elasticbeanstalk.com"
+    test_text = ["Donald Trump won the 2024 Election",  # False
+                "Toronto is the Capital of Ontario",    # True 
+                "The USSR exists today",                # False 
+                "Christmas is celebrated in december"]  # True
     test_results = []
 
-    # get predictions on sample test
-    for test in test_text: 
-        response = requests.post(url2, data={'text': test})
-        print(response, response.text)
-
     print("starting api calls")
+
     for i in range(100):
         for num, text in enumerate(test_text):
+            print(num, text)
             status_code, latency = api_call(url, text)
             test_results.append([num, text, latency])
-    
+
     print("finished api calls")
     print("starting writing results to csv")
     with open("test_results.csv", "w", newline='') as f: 
@@ -62,6 +60,6 @@ def test():
         writer.writerows(test_results)
     print("finished writing results to csv") 
     boxplot()
-
-if __name__ == "__main__":
-    test()
+    
+    if __name__ == "__main__":
+        test()
